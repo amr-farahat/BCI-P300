b7cf018a7050531f7e78194f2e031c7f84999487
amr-farahat <31440908+amr-farahat@users.noreply.github.com>
2018-07-01 20:52:19 +0200

    make the models configurable

--- intersubjective_training.py
+++ intersubjective_training.py
@@ -1,88 +1,135 @@
-
-from src import *
-from os import listdir
+_intersubjective
+from src import collect_data_intersubjective, resample_transform, intersubjective_training, save_resutls, intersubjective_shallow, finetune
+import os
 from os.path import isfile, join
 from keras.models import load_model, clone_model
+
 from keras.optimizers import SGD
-from keras.models import clone_model
-import csv
+import sys
+#import csv
+import pdb
+#%%
 
-## leave one out cross validation. (training on all the subjects data except one and test on him)
-
-# computing the signifcant channels based on the absolute value of the saliency maps.
-
-all_grads = np.load('plots/saliency_maps/all_grads_eegnet.npy')
-all_grads_mean = np.mean(all_grads, axis=0)
-all_grads_mean = np.swapaxes(all_grads_mean, 0, 1)[:,:,0]
-ch_significance = np.mean(np.abs(all_grads_mean), axis=1)
-eeglabel =  np.array(['Fz','Cz','Pz','Oz','Iz','Fp1','Fp2','F3','F4','F7','F8','T7','T8','C3','C4','P3','P4','O9','O10','P7',
-             'P8','FC1','FC2','CP1','CP2','PO3','PO4','PO7','PO8','LMAST'])
-channels_sorted = np.argsort(-ch_significance)
-significant_channels = channels_sorted[:5]
-insignificant_channels = channels_sorted[26:]
-significant_channels.sort()
-insignificant_channels.sort()
+#channels_sorted = np.load('plots/saliency_maps/channels_sorted.npy')
+#significant_channels = channels_sorted[:len(channels_sorted)/2]
+#insignificant_channels = channels_sorted[len(channels_sorted)/2:]
+#significant_channels.sort()
+#insignificant_channels.sort()
 
 subjects = [name for name in os.listdir("./data/50/subjects/")]
-test_subjects = subjects
+#test_subjects = subjects
+#test_subjects = ['ab82']
+test_subjects = [sys.argv[1]]
+# pdb.set_trace()
 
-# leave one subject out cross validation.
-for test_subject in test_subjects:
-    print 'working on subject', test_subject
-    #test_subject = subject
-    #collecting the data
-    x_train, y_train, x_test, y_test, o_t_test, o_tr_test = collect_data_intersubjective(subjects, test_subject, channels=[])
+batch_size = 64
+lr = 0.001
+early_stopping = True
+epochs = 500
+patience = 20
 
-    #rescaling and oversampling
-    x_train, y_train, x_test, y_test = resample_transform((x_train, y_train, x_test, y_test))
+model_config={'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'}
 
-    #Train
-    metrics, history, cnf_matrix = intersubjective_training((x_train, y_train, x_test, y_test, o_t_test, o_tr_test),
-                                                      epochs=100)
-    # saving the results.
-    super_final_results = save_resutls([metrics], [history], test_subject, suffix='intersubjective_bayesian_permenant20')
-    
-    # finetuning the model with varied number of subject's own trials.
-    model=clone_model(history.model)
-    weights = history.model.get_weights()
+ft = False
+ft_mode = 'all'
+ft_trials = [10, 20, 30, 40, 50, 60, 70]
+#
+#model_name = 'deep_branched_intersubjective-111-'+str(dataset)
+#dataset = '50_avg'
 
-    for i in [10, 20, 30, 50, 70]:
-        model.set_weights(weights)
-        metrics, history, cnf_matrix = finetune(model, (x_test, y_test, o_t_test, o_tr_test),
-                                        epochs=500, train_trials=i , mode='all')
-
-        super_final_results = save_resutls([metrics], [history], test_subject, suffix='intersubjective_ft_'+str(i)+'eegnet')
+datasets = ['50_avg', '250']
 
 
+for dataset in datasets:
+    model_names = ['deep_intersubjective_branched_'+str(dataset)+'_thesis1',
+              'deep_intersubjective_eegnet_'+str(dataset)+'_thesis1',
+              'deep_intersubjective_cnn_'+str(dataset)+'_thesis1',
+              'lda_intersubjective_shrinkage_'+str(dataset)+'_thesis1',
+              'lda_intersubjective_'+str(dataset)+'_thesis1',
+              'deep_intersubjective_branched_no_bn_'+str(dataset)+'_thesis1',
+              'deep_intersubjective_branched_no_dropout_'+str(dataset)+'_thesis1',
+              'deep_intersubjective_branched_no_branched_'+str(dataset)+'_thesis1',
+              'deep_intersubjective_branched_no_deep_'+str(dataset)+'_thesis1',
+              'deep_intersubjective_branched_relu_'+str(dataset)+'_thesis1',
+              'deep_intersubjective_branched_elu_'+str(dataset)+'_thesis1']
 
-# collecting the saved results of a certain exdperiment and creating a csv file with the results of the metric
-# that we are interested in.
-trial_names = ['intersubjective_bayesian_permenant20']
-metric = 'val_recognition_acc'
-g_mus = []
-for trial_name in trial_names:
-    path = './results/subjects/'
-    subjects = [name for name in os.listdir(path)]
-    mus = []
-    stds=[]
-    for subject in subjects:
-        files = [f for f in listdir(join(path, subject)) if isfile(join(path, subject, f))]
-        myfile = [file for file in files if trial_name in file ][0]
-        d = np.load(join(path, subject, myfile))
-        sfr = d['super_final_results']
-        mu = sfr[0][metric]['mean']
-        std = sfr[0][metric]['std']
-        mus.append(mu)
-        stds.append(std)
-#     g_mu = np.mean([u*100 for u in mus])
-    g_mus.append([round(ff*100,2) for ff in mus])
+    model_configs = [{'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':False, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':False, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':False, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':False, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'relu'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'elu'}]
 
-g_mus = np.array(g_mus)
-g_mus = g_mus.T
-subjects = np.array(subjects)
-subjects = np.expand_dims(subjects, axis=1)
 
-with open('temp.csv', 'wb') as myfile:
-    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-    wr.writerow(['']+trial_names)
-    wr.writerows(np.hstack((subjects,g_mus)))
+    for model_name, model_config in zip(model_names, model_configs):
+
+
+        for test_subject in test_subjects:
+            print 'working on subject', test_subject
+            #test_subject = subject
+            #collecting the data
+            print 'Collecting data ...'
+            x_train, y_train, x_test, y_test, o_t_test, o_tr_test = collect_data_intersubjective(subjects,
+                                                                                                 test_subject,
+                                                                                                 mode='eeg',
+                                                                                                 channels=range(29),
+                                                                                             frequency=dataset)
+
+
+            #Train
+            print "Training ..."
+            if model_name.startswith('deep'):
+                metrics, history, cnf_matrix = intersubjective_training((x_train, y_train, x_test, y_test, o_t_test, o_tr_test),
+                                                                        model_name,
+                                                                        test_subject,
+                                                                        epochs=epochs,
+                                                                        lr=lr,
+                                                                        batch_size=batch_size,
+                                                                        model_config=model_config,
+                                                                        early_stopping=early_stopping,
+                                                                        patience=patience)
+                super_final_results = save_resutls([metrics], [history], test_subject,
+                                                   suffix=model_name,
+                                                   early_stopping=early_stopping,
+                                                   patience=patience)
+            else:
+                metrics, history, cnf_matrix, clf = intersubjective_shallow((x_train, y_train, x_test, y_test, o_t_test, o_tr_test),
+                                                                       model_name)
+                super_final_results = save_resutls(metrics, history, test_subject, suffix=model_name, clf=clf)
+
+            print 'DA:', model_name,'_', test_subject, ' is:', super_final_results[0]['val_recognition_acc']['mean']
+            print 'BA:', model_name,'_', test_subject, ' is:', super_final_results[0]['val_balanced_acc']['mean']
+            print 'Recall:', model_name,'_', test_subject, ' is:', super_final_results[0]['val_recalls']['mean']
+            print 'precision:', model_name,'_', test_subject, ' is:', super_final_results[0]['val_precisions']['mean']
+
+            if model_name.startswith('deep_intersubjective_branched') and ft:
+                model=clone_model(history.model)
+                weights = history.model.get_weights()
+
+                for i in ft_trials:
+                    model_name_modified = 'deep_intersubjective_branched_ft_'+str(i)+'_trials_'+str(dataset)+'_thesis2'
+                    model_name_modified = model_name+'_ft_'+str(i)+'_trials'
+                    model.set_weights(weights)
+                    metrics, history, cnf_matrix = finetune(model,
+                                                            (x_test, y_test, o_t_test, o_tr_test),
+                                                            model_name_modified,
+                                                            test_subject,
+                                                            epochs=epochs,
+                                                            train_trials=i,
+                                                            mode=ft_mode,
+                                                            early_stopping=early_stopping,
+                                                            patience=patience)
+
+                    super_final_results = save_resutls([metrics], [history], test_subject,
+                                                   suffix=model_name_modified,
+                                                   early_stopping=early_stopping,
+                                                   patience=patience)
+                    print 'DA:', model_name_modified,'_', test_subject, ' is:', super_final_results[0]['val_recognition_acc']['mean']
+                    print 'BA:', model_name_modified,'_', test_subject, ' is:', super_final_results[0]['val_balanced_acc']['mean']
+                    print 'Recall:', model_name_modified,'_', test_subject, ' is:', super_final_results[0]['val_recalls']['mean']
+                    print 'precision:', model_name_modified,'_', test_subject, ' is:', super_final_results[0]['val_precisions']['mean']
--- subjective_training.py
+++ subjective_training.py
@@ -1,52 +1,78 @@
 
-from src import *
+
+from src import load_data, preprocessing, cross_validator, save_resutls
+
+import matplotlib.pyplot as plt
 from os import listdir
 from os.path import isfile, join
-import csv
+from utils import plot_confusion_matrix
+import sys
+import pdb
+#%%
 
-# training on only the subject's data with 5 fold cross validation.
+eeglabel =  ['Fz','Cz','Pz','Oz','Iz','Fp1','Fp2','F3','F4','F7','F8','T7','T8','C3','C4','P3','P4','O9','O10','P7',
+             'P8','FC1','FC2','CP1','CP2','PO3','PO4','PO7','PO8','LMAST']
 
+subjects = [sys.argv[1]]
+#subjects = ['kq84']
 
-subjects = [name for name in os.listdir("./data/50/subjects/")]
-results = []
-
-for subject in subjects:
-    x, y, t, tr = load_data(subject)
-    x = preprocessing(x)
-    metrics, histories, cnf_matrices = cross_validator((x, y, t, tr), n_splits=5, epochs=500)
-
-    super_final_results = save_resutls(metrics, histories, subject, suffix='eegnet_subjective')
-
-    print 'working on subject : ', subject
-
+batch_size = 64
+lr = 0.001
+early_stopping=True
+patience=20
+epochs=500
+model_config={'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'}
 
 
 
-trial_names = ['eegnet_subjective']
-g_mus = []
-for trial_name in trial_names:
-    path = './results/subjects/'
-    subjects = [name for name in os.listdir(path)]
-    metric = 'val_recognition_acc'
-    mus = []
-    stds=[]
-    for subject in subjects:
-        files = [f for f in listdir(join(path, subject)) if isfile(join(path, subject, f))]
-        myfile = [file for file in files if trial_name in file ][0]
-        d = np.load(join(path, subject, myfile))
-        sfr = d['super_final_results']
-        mu = sfr[0][metric]['mean']
-        std = sfr[0][metric]['std']
-        mus.append(mu)
-        stds.append(std)
-    g_mus.append([round(ff*100,2) for ff in mus])
+datasets = ['50_avg','250']
 
-g_mus = np.array(g_mus)
-g_mus = g_mus.T
-subjects = np.array(subjects)
-subjects = np.expand_dims(subjects, axis=1)
 
-with open('temp.csv', 'wb') as myfile:
-    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-    wr.writerow(['']+trial_names)
-    wr.writerows(np.hstack((subjects,g_mus)))
+for dataset in datasets:
+    model_names = ['deep_subjective_branched_'+str(dataset)+'_thesis1',
+              'deep_subjective_eegnet_'+str(dataset)+'_thesis1',
+              'deep_subjective_cnn_'+str(dataset)+'_thesis1',
+              'lda_subjective_shrinkage_'+str(dataset)+'_thesis1',
+              'lda_subjective_'+str(dataset)+'_thesis1',
+              'deep_subjective_branched_no_bn_'+str(dataset)+'_thesis1',
+              'deep_subjective_branched_no_dropout_'+str(dataset)+'_thesis1',
+              'deep_subjective_branched_no_branched_'+str(dataset)+'_thesis1',
+              'deep_subjective_branched_no_deep_'+str(dataset)+'_thesis1',
+              'deep_subjective_branched_relu_'+str(dataset)+'_thesis1',
+              'deep_subjective_branched_elu_'+str(dataset)+'_thesis1']
+
+    model_configs = [{'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':False, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':False, 'branched':True, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':False, 'deep':True, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':False, 'nonlinear':'tanh'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'relu'},
+                    {'bn':True, 'dropout':True, 'branched':True, 'deep':True, 'nonlinear':'elu'}]
+
+
+    for model_name, model_config in zip(model_names, model_configs):
+        for subject in subjects:
+
+            print 'working on subject : ', subject
+            x, y, t, tr = load_data(subject, channels=range(29), frequency=dataset)
+            x = preprocessing(x, frequency=dataset)
+            metrics, histories, cnf_matrices = cross_validator((x, y, t, tr),subject,
+                                                               n_splits=5, epochs=epochs,
+                                                               batch_size=batch_size,
+                                                               lr=lr,
+                                                               model_name=model_name,
+                                                               model_config=model_config,
+                                                               early_stopping=early_stopping,
+                                                               patience=patience)
+
+            super_final_results = save_resutls(metrics, histories, subject, suffix=model_name, early_stopping=early_stopping,
+                                               patience=patience)
+
+            print 'DA:', model_name,'_', subject, ' is:', super_final_results[0]['val_recognition_acc']['mean']
+            print 'BA:', model_name,'_', subject, ' is:', super_final_results[0]['val_balanced_acc']['mean']
+            print 'Recall:', model_name,'_', subject, ' is:', super_final_results[0]['val_recalls']['mean']
+            print 'precision:', model_name,'_', subject, ' is:', super_final_results[0]['val_precisions']['mean']
